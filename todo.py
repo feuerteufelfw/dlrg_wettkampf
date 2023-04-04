d9ff7b9c6204f1f3529aa7f52591daf4f85cdc78
feuerteufelfw <feuerteufel.fw@gmail.com>
2023-04-04 17:02:02 +0200

    to do list aktualisiert, angefangen in in line css auf css datei zu wechseln

--- todo.py
+++ todo.py
@@ -1,7 +1,16 @@
 #zeiten stoppen home button
+auswertung position ermittlung geht nicht (orientirung der position an der ersten zahl der Zeit s)
+export button geht nicht (momentan muss für den export eine Disziplin angegeben werden, im webinterface exestiert dieses feld aber nicht mehr )
 zeiten stoppen Zeit anzeigen (extra website)
-speicher kein zeiten file
-zuletzt angekommende schwimmer anzeigen
-laden aus config filr
-reset button
-neue teilnehmer zu bestehenden hinzufügen
+zeit stoppen website erkennen ob zeit schon läuft und reagieren
+stoppuhr resetten
+html tabelle mit teilnehmern scrollbar machen
+extra webiste mit angekommenden schwimmern (eventuell direkt mit ergebnis ?)
+auswertung test
+export test
+auswertung pro disziplin
+intern von csv speicherung auf speicherung in db wechseln
+export von ergebnis tabelle xlsx
+export von teilnehmer tabelle xlsx
+test mehrer zugriffe gleichzeitig testen
+password zugriff ?
